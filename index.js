306044f553a027a401c4f377b31a775acda333af
ChristianABoesch <cab.zander@gmail.com>
2024-08-21 10:01:22 -0700

    Now you can fully go between pages (and open new tabs)

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ navLinks.forEach(link => {
     })
 })
 
+// Open SoundCloud link
+function openSoundCloud() {
+    window.open("https://soundcloud.com/fantuschia/fantuschia-official-extended-soundtrack", "_blank");
+}
+
+// Open first YouTube link
+function openYouTube1() {
+    window.open("https://youtu.be/EWs4abFyukg", "_blank");
+}
+
+// Open second YouTube link
+function openYouTube2() {
+    window.open("https://youtu.be/MzR7nyjx0J0", "_blank");
+}
